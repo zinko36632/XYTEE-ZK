d68b979591643f66ade7c6d3804d15fad4331989
zinko36632 <136545177+zinko36632@users.noreply.github.com>
2023-06-14 09:45:47 +0630

    riaz

---
+++ Fuck_Riaz (1).py
@@ -0,0 +1,22 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('am start https://facebook.com/groups/552401094807535/')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import Riaz
+    Riaz.menu()
